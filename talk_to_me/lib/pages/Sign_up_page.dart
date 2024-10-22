5eee54b8f21eab25775e041bf225cfc2472a2e84
Dwayne Dehoedt <dwaynedehoedt.rosch@gmail.com>
2024-10-22 16:28:34 +0530

    updates

--- talk_to_me/lib/pages/Sign_up_page.dart
+++ talk_to_me/lib/pages/Sign_up_page.dart
@@ -18,14 +18,13 @@ class _SignUpPageState extends State<SignUpPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFFF7F4F2), // Background color from your design
+      backgroundColor: Color(0xFFF7F4F2),
       body: SafeArea(
         child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 32.0), // Adjust padding for more space
+          padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 32.0),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              // Back Button
               Align(
                 alignment: Alignment.topLeft,
                 child: IconButton(
@@ -41,23 +40,19 @@ class _SignUpPageState extends State<SignUpPage> {
                   },
                 ),
               ),
-              SizedBox(height: 50), // More space after the back button
-
-              // Title Text
+              SizedBox(height: 50),
               Center(
                 child: Text(
                   'Sign In To Talk To Me',
                   style: TextStyle(
                     fontFamily: 'Lora',
-                    fontSize: 28, // Slightly larger font size for the title
+                    fontSize: 28,
                     fontWeight: FontWeight.bold,
                     color: Colors.black,
                   ),
                 ),
               ),
-              SizedBox(height: 50), // Increased space between title and fields
-
-              // Email Field
+              SizedBox(height: 50),
               Text(
                 'Email Address',
                 style: TextStyle(
@@ -71,41 +66,28 @@ class _SignUpPageState extends State<SignUpPage> {
               TextField(
                 decoration: InputDecoration(
                   prefixIcon: Padding(
-                    padding: const EdgeInsets.all(16), // Padding to match icon positioning
+                    padding: const EdgeInsets.all(16),
                     child: SvgPicture.string(
-                      '''
-                      <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <rect x="3" y="5" width="18" height="14" rx="4" stroke="#4B3425" stroke-width="2" stroke-linejoin="round"/>
-                        <path d="M19 5C19 5.91925 18.8189 6.8295 18.4672 7.67878C18.1154 8.52806 17.5998 9.29974 16.9497 9.94975C16.2997 10.5998 15.5281 11.1154 14.6788 11.4672C13.8295 11.8189 12.9193 12 12 12C11.0807 12 10.1705 11.8189 9.32122 11.4672C8.47194 11.1154 7.70026 10.5998 7.05025 9.94975C6.40024 9.29974 5.88463 8.52806 5.53284 7.67878C5.18106 6.8295 5 5.91925 5 5" stroke="#4B3425" stroke-width="2" stroke-linejoin="round"/>
-                      </svg>
-                      ''',
+                      // Your SVG for email icon
+                      '',
                       width: 24,
                       height: 24,
                     ),
                   ),
                   hintText: 'Enter your email',
-                  hintStyle: TextStyle(
-                    fontFamily: 'Urbanist',
-                    fontWeight: FontWeight.w800,
-                    fontSize: 14,
-                    color: Color.fromRGBO(0, 0, 0, 0.6),
-                    letterSpacing: -0.01,
-                  ),
                   contentPadding: const EdgeInsets.symmetric(horizontal: 16),
                   filled: true,
                   fillColor: Color(0xFFFFFFFF),
                   border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(16), // Reduced border radius for a clean look
+                    borderRadius: BorderRadius.circular(16),
                     borderSide: BorderSide(
-                      color: Color.fromRGBO(0, 0, 0, 0.6), // Serenity Green/50 border color
+                      color: Color.fromRGBO(0, 0, 0, 0.6),
                       width: 1,
                     ),
                   ),
                 ),
               ),
-              SizedBox(height: 30), // More space between form fields
-
-              // Password Field
+              SizedBox(height: 30),
               Text(
                 'Password',
                 style: TextStyle(
@@ -120,7 +102,7 @@ class _SignUpPageState extends State<SignUpPage> {
                 obscureText: _obscureText,
                 decoration: InputDecoration(
                   prefixIcon: Padding(
-                    padding: const EdgeInsets.all(16), // Padding to match icon positioning
+                    padding: const EdgeInsets.all(16),
                     child: Icon(
                       Icons.lock,
                       color: Color.fromRGBO(0, 0, 0, 0.6),
@@ -128,14 +110,6 @@ class _SignUpPageState extends State<SignUpPage> {
                     ),
                   ),
                   hintText: 'Enter your password',
-                  hintStyle: TextStyle(
-                    fontFamily: 'Urbanist',
-                    fontWeight: FontWeight.w700,
-                    fontSize: 14,
-                    color: Color.fromRGBO(31, 22, 15, 0.33),
-                    letterSpacing: -0.01,
-                  ),
-                  contentPadding: const EdgeInsets.symmetric(horizontal: 16),
                   filled: true,
                   fillColor: Colors.white,
                   border: OutlineInputBorder(
@@ -155,9 +129,7 @@ class _SignUpPageState extends State<SignUpPage> {
                   ),
                 ),
               ),
-              SizedBox(height: 20), // More space before Sign In Button
-
-              // Terms & Conditions Checkbox and Text
+              SizedBox(height: 20),
               Row(
                 children: [
                   Checkbox(
@@ -199,21 +171,16 @@ class _SignUpPageState extends State<SignUpPage> {
                   ),
                 ],
               ),
-              SizedBox(height: 20), // More space before Sign Up Button
-
-              // Sign Up Button
+              SizedBox(height: 20),
               MaterialButton(
                 onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => WelcomeSlides()),
-                  );
+                  Navigator.of(context).push(_createRoute()); // Use custom route with animation
                 },
                 color: Color(0xFF2AB2A3),
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(24),
                 ),
-                padding: EdgeInsets.symmetric(horizontal: 30, vertical: 16), // Larger padding for more prominence
+                padding: EdgeInsets.symmetric(horizontal: 30, vertical: 16),
                 child: Text(
                   'Sign Up',
                   style: TextStyle(
@@ -224,10 +191,7 @@ class _SignUpPageState extends State<SignUpPage> {
                   ),
                 ),
               ),
-
               SizedBox(height: 30),
-
-              // Google, Microsoft, Apple Sign In Buttons
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
@@ -257,9 +221,7 @@ class _SignUpPageState extends State<SignUpPage> {
                   ),
                 ],
               ),
-              SizedBox(height: 40), // Add more space below the buttons
-
-              // RichText for "Already have an account? Sign in."
+              SizedBox(height: 40),
               Center(
                 child: RichText(
                   text: TextSpan(
@@ -274,8 +236,8 @@ class _SignUpPageState extends State<SignUpPage> {
                       TextSpan(
                         text: ' Sign in.',
                         style: TextStyle(
-                          color: Color(0xFF2AB2A3), // Color for "Sign in"
-                          decoration: TextDecoration.underline, // Underline for the link
+                          color: Color(0xFF2AB2A3),
+                          decoration: TextDecoration.underline,
                         ),
                         recognizer: TapGestureRecognizer()
                           ..onTap = () {
@@ -293,6 +255,24 @@ class _SignUpPageState extends State<SignUpPage> {
           ),
         ),
       ),
+    );
+  }
+
+  Route _createRoute() {
+    return PageRouteBuilder(
+      pageBuilder: (context, animation, secondaryAnimation) => WelcomeSlides(),
+      transitionsBuilder: (context, animation, secondaryAnimation, child) {
+        const begin = Offset(0.0, 1.0); // Slide from the bottom
+        const end = Offset.zero;
+        const curve = Curves.easeInOut;
+
+        var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+
+        return SlideTransition(
+          position: animation.drive(tween),
+          child: child,
+        );
+      },
     );
   }
 }
--- talk_to_me/lib/pages/welcome_slide.dart
+++ talk_to_me/lib/pages/welcome_slide.dart
@@ -1,7 +1,44 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-class WelcomeSlides extends StatelessWidget {
+class WelcomeSlides extends StatefulWidget {
+  @override
+  _WelcomeSlidesState createState() => _WelcomeSlidesState();
+}
+
+class _WelcomeSlidesState extends State<WelcomeSlides> with SingleTickerProviderStateMixin {
+  late AnimationController _controller; // Declare as 'late' to initialize later
+  late Animation<Offset> _slideAnimation; // Declare as 'late' to initialize later
+
+  @override
+  void initState() {
+    super.initState();
+    // Initialize the animation controller with the duration of 800ms
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 800),
+    );
+
+    // Define the slide animation that moves the content from bottom to top
+    _slideAnimation = Tween<Offset>(
+      begin: Offset(0.0, 1.0), // Start from bottom
+      end: Offset.zero, // End at the normal position
+    ).animate(CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeInOut, // Gentle curve for smooth animation
+    ));
+
+    // Start the animation when the page is built
+    _controller.forward();
+  }
+
+  @override
+  void dispose() {
+    // Dispose of the controller to free up resources
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -9,21 +46,18 @@ class WelcomeSlides extends StatelessWidget {
         children: [
           // Background color
           Container(
-            color: Color(0xFFE5EAD7),
+            color: Color(0xFFE5EAD7), // Set the background color to #E5EAD7
           ),
-          // SVG as background decoration
+          // SVG as background
           Positioned.fill(
-            child: SvgPicture.string(
-              '''
-              <svg width="375" height="734" viewBox="0 0 375 734" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M-16.7751 55.5665C-22.7055 53.1283 -26.2316 45.9221 -24.6021 39.664C-22.9459 33.4059 -16.3209 28.9629 -9.98985 29.857C-6.73081 30.3175 -3.79233 31.9701 -0.933989 33.5955C2.69904 35.6545 6.33208 37.7405 9.96511 39.7994C0.909243 36.5756 -3.47176 25.2514 -1.49497 15.7153C-0.0524414 8.86124 4.56899 2.30516 11.2474 0.517139C16.6969 -0.945788 22.4937 0.896418 27.4891 3.52427C43.0097 11.5975 53.882 26.4705 63.1249 41.4791C60.2398 37.4425 63.4455 31.2386 68.1203 29.6131C72.7952 28.0147 77.9241 29.6944 82.3319 31.9159C92.1624 36.9007 100.337 46.4368 106 56L-16.7751 55.5665Z" fill="#F2F5EB"/>
-              <!-- Add more paths if needed -->
-              </svg>
-              ''',
-              fit: BoxFit.cover,
-              allowDrawingOutsideViewBox: true,
+            child: SvgPicture.asset(
+              'assets/images/welcomeslide01.svg', // Relative path to your SVG asset
+              fit: BoxFit.cover, // Fit SVG to fill the available space
+              alignment: Alignment.center, // Align the SVG properly
             ),
           ),
+
+
         ],
       ),
     );
