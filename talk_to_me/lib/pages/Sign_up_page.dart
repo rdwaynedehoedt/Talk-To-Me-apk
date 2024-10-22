001a5ae27d6d89a2fea2d5d969f66b9a2f9c0154
Dwayne Dehoedt <dwaynedehoedt.rosch@gmail.com>
2024-10-22 16:31:47 +0530

    updates

--- talk_to_me/lib/pages/Sign_up_page.dart
+++ talk_to_me/lib/pages/Sign_up_page.dart
@@ -18,240 +18,257 @@ class _SignUpPageState extends State<SignUpPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFFF7F4F2),
+      backgroundColor: Color(0xFFF7F4F2), // Background color from your design
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 32.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Align(
-                alignment: Alignment.topLeft,
-                child: IconButton(
-                  icon: SvgPicture.asset(
-                    'assets/images/backbutton.svg',
-                    width: 24,
-                    height: 24,
-                  ),
-                  onPressed: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => WelcomeScreen())
-                    );
-                  },
-                ),
-              ),
-              SizedBox(height: 50),
-              Center(
-                child: Text(
-                  'Sign In To Talk To Me',
-                  style: TextStyle(
-                    fontFamily: 'Lora',
-                    fontSize: 28,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black,
-                  ),
-                ),
-              ),
-              SizedBox(height: 50),
-              Text(
-                'Email Address',
-                style: TextStyle(
-                  fontFamily: 'Urbanist',
-                  fontWeight: FontWeight.bold,
-                  fontSize: 16,
-                  color: Colors.black,
-                ),
-              ),
-              SizedBox(height: 8),
-              TextField(
-                decoration: InputDecoration(
-                  prefixIcon: Padding(
-                    padding: const EdgeInsets.all(16),
-                    child: SvgPicture.string(
-                      // Your SVG for email icon
-                      '',
+        child: SingleChildScrollView( // Wrap the content in SingleChildScrollView to avoid overflow
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 32.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                // Back Button
+                Align(
+                  alignment: Alignment.topLeft,
+                  child: IconButton(
+                    icon: SvgPicture.asset(
+                      'assets/images/backbutton.svg', // Ensure this SVG file exists in your assets folder
                       width: 24,
                       height: 24,
                     ),
-                  ),
-                  hintText: 'Enter your email',
-                  contentPadding: const EdgeInsets.symmetric(horizontal: 16),
-                  filled: true,
-                  fillColor: Color(0xFFFFFFFF),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(16),
-                    borderSide: BorderSide(
-                      color: Color.fromRGBO(0, 0, 0, 0.6),
-                      width: 1,
-                    ),
-                  ),
-                ),
-              ),
-              SizedBox(height: 30),
-              Text(
-                'Password',
-                style: TextStyle(
-                  fontFamily: 'Urbanist',
-                  fontWeight: FontWeight.bold,
-                  fontSize: 16,
-                  color: Colors.black,
-                ),
-              ),
-              SizedBox(height: 8),
-              TextField(
-                obscureText: _obscureText,
-                decoration: InputDecoration(
-                  prefixIcon: Padding(
-                    padding: const EdgeInsets.all(16),
-                    child: Icon(
-                      Icons.lock,
-                      color: Color.fromRGBO(0, 0, 0, 0.6),
-                      size: 24,
-                    ),
-                  ),
-                  hintText: 'Enter your password',
-                  filled: true,
-                  fillColor: Colors.white,
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(16),
-                    borderSide: BorderSide.none,
-                  ),
-                  suffixIcon: IconButton(
-                    icon: Icon(
-                      _obscureText ? Icons.visibility_off : Icons.visibility,
-                      color: Color.fromRGBO(0, 0, 0, 0.6),
-                    ),
                     onPressed: () {
-                      setState(() {
-                        _obscureText = !_obscureText;
-                      });
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => WelcomeScreen()),
+                      );
                     },
                   ),
                 ),
-              ),
-              SizedBox(height: 20),
-              Row(
-                children: [
-                  Checkbox(
-                    value: _termsAccepted,
-                    onChanged: (bool? value) {
-                      setState(() {
-                        _termsAccepted = value ?? false;
-                      });
-                    },
-                  ),
-                  Expanded(
-                    child: RichText(
-                      text: TextSpan(
-                        text: 'I Agree with the ',
-                        style: TextStyle(
-                          fontFamily: 'Urbanist',
-                          fontSize: 14,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black,
-                        ),
-                        children: <TextSpan>[
-                          TextSpan(
-                            text: 'Terms & Conditions',
-                            style: TextStyle(
-                              color: Color(0xFF2AB2A3),
-                              decoration: TextDecoration.underline,
-                            ),
-                            recognizer: TapGestureRecognizer()
-                              ..onTap = () {
-                                Navigator.push(
-                                  context,
-                                  MaterialPageRoute(builder: (context) => TandC()),
-                                );
-                              },
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(height: 20),
-              MaterialButton(
-                onPressed: () {
-                  Navigator.of(context).push(_createRoute()); // Use custom route with animation
-                },
-                color: Color(0xFF2AB2A3),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(24),
-                ),
-                padding: EdgeInsets.symmetric(horizontal: 30, vertical: 16),
-                child: Text(
-                  'Sign Up',
-                  style: TextStyle(
-                    fontFamily: 'Urbanist',
-                    fontSize: 18,
-                    fontWeight: FontWeight.w800,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-              SizedBox(height: 30),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  IconButton(
-                    onPressed: () {},
-                    icon: SvgPicture.asset(
-                      'assets/images/microsoft_icon.svg',
-                      width: 40,
-                      height: 40,
-                    ),
-                  ),
-                  IconButton(
-                    onPressed: () {},
-                    icon: SvgPicture.asset(
-                      'assets/images/google_icon.svg',
-                      width: 40,
-                      height: 40,
-                    ),
-                  ),
-                  IconButton(
-                    onPressed: () {},
-                    icon: SvgPicture.asset(
-                      'assets/images/apple_icon.svg',
-                      width: 40,
-                      height: 40,
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(height: 40),
-              Center(
-                child: RichText(
-                  text: TextSpan(
-                    text: 'Already have an account?',
+                SizedBox(height: 50),
+
+                // Title Text
+                Center(
+                  child: Text(
+                    'Sign In To Talk To Me',
                     style: TextStyle(
-                      fontFamily: 'Urbanist',
-                      fontSize: 14,
+                      fontFamily: 'Lora',
+                      fontSize: 28,
                       fontWeight: FontWeight.bold,
                       color: Colors.black,
                     ),
-                    children: <TextSpan>[
-                      TextSpan(
-                        text: ' Sign in.',
-                        style: TextStyle(
-                          color: Color(0xFF2AB2A3),
-                          decoration: TextDecoration.underline,
-                        ),
-                        recognizer: TapGestureRecognizer()
-                          ..onTap = () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(builder: (context) => SignInPage()),
-                            );
-                          },
-                      ),
-                    ],
                   ),
                 ),
-              ),
-            ],
+                SizedBox(height: 50),
+
+                // Email Field
+                Text(
+                  'Email Address',
+                  style: TextStyle(
+                    fontFamily: 'Urbanist',
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16,
+                    color: Colors.black,
+                  ),
+                ),
+                SizedBox(height: 8),
+                TextField(
+                  decoration: InputDecoration(
+                    prefixIcon: Padding(
+                      padding: const EdgeInsets.all(16),
+                      child: SvgPicture.asset(
+                        'assets/images/email_icon.svg', // Update the SVG asset as needed
+                        width: 24,
+                        height: 24,
+                      ),
+                    ),
+                    hintText: 'Enter your email',
+                    contentPadding: const EdgeInsets.symmetric(horizontal: 16),
+                    filled: true,
+                    fillColor: Color(0xFFFFFFFF),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(16),
+                      borderSide: BorderSide(
+                        color: Color.fromRGBO(0, 0, 0, 0.6),
+                        width: 1,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 30),
+
+                // Password Field
+                Text(
+                  'Password',
+                  style: TextStyle(
+                    fontFamily: 'Urbanist',
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16,
+                    color: Colors.black,
+                  ),
+                ),
+                SizedBox(height: 8),
+                TextField(
+                  obscureText: _obscureText,
+                  decoration: InputDecoration(
+                    prefixIcon: Padding(
+                      padding: const EdgeInsets.all(16),
+                      child: Icon(
+                        Icons.lock,
+                        color: Color.fromRGBO(0, 0, 0, 0.6),
+                        size: 24,
+                      ),
+                    ),
+                    hintText: 'Enter your password',
+                    filled: true,
+                    fillColor: Colors.white,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(16),
+                      borderSide: BorderSide.none,
+                    ),
+                    suffixIcon: IconButton(
+                      icon: Icon(
+                        _obscureText ? Icons.visibility_off : Icons.visibility,
+                        color: Color.fromRGBO(0, 0, 0, 0.6),
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          _obscureText = !_obscureText;
+                        });
+                      },
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20),
+
+                // Terms & Conditions Checkbox and Text
+                Row(
+                  children: [
+                    Checkbox(
+                      value: _termsAccepted,
+                      onChanged: (bool? value) {
+                        setState(() {
+                          _termsAccepted = value ?? false;
+                        });
+                      },
+                    ),
+                    Expanded(
+                      child: RichText(
+                        text: TextSpan(
+                          text: 'I Agree with the ',
+                          style: TextStyle(
+                            fontFamily: 'Urbanist',
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                          ),
+                          children: <TextSpan>[
+                            TextSpan(
+                              text: 'Terms & Conditions',
+                              style: TextStyle(
+                                color: Color(0xFF2AB2A3),
+                                decoration: TextDecoration.underline,
+                              ),
+                              recognizer: TapGestureRecognizer()
+                                ..onTap = () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(builder: (context) => TandC()),
+                                  );
+                                },
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 20),
+
+                // Sign Up Button
+                MaterialButton(
+                  onPressed: () {
+                    Navigator.of(context).push(_createRoute()); // Custom route with animation
+                  },
+                  color: Color(0xFF2AB2A3),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(24),
+                  ),
+                  padding: EdgeInsets.symmetric(horizontal: 30, vertical: 16),
+                  child: Text(
+                    'Sign Up',
+                    style: TextStyle(
+                      fontFamily: 'Urbanist',
+                      fontSize: 18,
+                      fontWeight: FontWeight.w800,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                SizedBox(height: 30),
+
+                // Google, Microsoft, Apple Sign In Buttons
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    IconButton(
+                      onPressed: () {},
+                      icon: SvgPicture.asset(
+                        'assets/images/microsoft_icon.svg',
+                        width: 40,
+                        height: 40,
+                      ),
+                    ),
+                    IconButton(
+                      onPressed: () {},
+                      icon: SvgPicture.asset(
+                        'assets/images/google_icon.svg',
+                        width: 40,
+                        height: 40,
+                      ),
+                    ),
+                    IconButton(
+                      onPressed: () {},
+                      icon: SvgPicture.asset(
+                        'assets/images/apple_icon.svg',
+                        width: 40,
+                        height: 40,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 40),
+
+                // RichText for "Already have an account? Sign in."
+                Center(
+                  child: RichText(
+                    text: TextSpan(
+                      text: 'Already have an account?',
+                      style: TextStyle(
+                        fontFamily: 'Urbanist',
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
+                      children: <TextSpan>[
+                        TextSpan(
+                          text: ' Sign in.',
+                          style: TextStyle(
+                            color: Color(0xFF2AB2A3),
+                            decoration: TextDecoration.underline,
+                          ),
+                          recognizer: TapGestureRecognizer()
+                            ..onTap = () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(builder: (context) => SignInPage()),
+                              );
+                            },
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
